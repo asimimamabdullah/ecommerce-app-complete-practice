5cbfb7431b4338e0f30834c76e933695574580cc
asimimamabdullah <asimimamabdullah@gmail.com>
2021-08-01 13:24:49 +0500

    day 1 --L aug 1 21

--- client/src/Components/mainpages/detailProduct/DetailProduct.jsx
+++ client/src/Components/mainpages/detailProduct/DetailProduct.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const DetailProduct = () => {
-  return <div>DetailProduct change</div>;
+  return <div>DetailProduct change again</div>;
 };
 
 export default DetailProduct;
